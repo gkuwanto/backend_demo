2f6309f69a120b6e81635613aa328931a1de2276
gkuwanto <37175707+gkuwanto@users.noreply.github.com>
2023-01-18 01:44:29 +0000

    add optional

--- schemas.py
+++ schemas.py
@@ -1,4 +1,4 @@
-from typing import List, Union
+from typing import List, Union, Optional
 
 from pydantic import BaseModel
 
@@ -9,9 +9,9 @@ class JobBase(BaseModel):
 class JobCreate(JobBase):
     left_language_id: str
     right_language_id: str
-    monolingual_left_uploadpath: str
-    monolingual_right_uploadpath: str
-    parallel_uploadpath: str
+    monolingual_left_uploadpath: Optional[str] = None
+    monolingual_right_uploadpath: Optional[str] = None
+    parallel_uploadpath: Optional[str] = None
     word_dictionary_uploadpath: str
     validation_uploadpath: str
     test_uploadpath: str
